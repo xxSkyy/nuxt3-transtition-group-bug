37067a9fbd9b341ef796e9fad02e1770b7aa7931
xxSkyy <michaaux3@gmail.com>
2021-12-13 18:24:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reproduction for Nuxt3 transition-group has no classes
+
+It only shows on hot reload in dev mode when you'll change class name
+
+on build-start it doesn't work also
---
+++ app.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+const list = ["Welcome", "to", "nuxt3"]
+</script>
+
+<template>
+  <div class="box">
+    <div class="wrapper">
+      <h1>List test</h1>
+      <transition-group class="test" name="fade" tag="ul">
+        <li v-for="i in list" :id="i">{{ i }}</li>
+      </transition-group>
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+body {
+  background-color: rgb(27, 27, 27);
+  color: white;
+}
+</style>
+
+<style scoped>
+.box {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.wrapper {
+  background-color: rgb(36, 51, 99);
+  border-radius: 2rem;
+  padding: 2rem;
+}
+
+ul {
+  list-style-type: none;
+  background-color: turquoise;
+}
+
+li {
+  text-align: center;
+  margin: 1rem 0;
+}
+
+h1 {
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
